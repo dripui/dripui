9771d78600f5ddda5db35463934da98e85c7b1be
Yam Borodetsky <hi@yam.codes>
2023-11-24 22:36:14 +0200

    fix: package bundle issues preventing import

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
 import dts from 'vite-plugin-dts';
+import packageJson from "./package.json";
 
+const { name } = packageJson;
 const entryRoot = resolve("src", "components");
 const entry = resolve(entryRoot, "index.ts");
 
@@ -17,8 +19,9 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry,
-      formats: ["es", "cjs", "umd"],
-      name: "@yamcodes/ui"
+      formats: ["es"],
+      name,
+      fileName: "index"
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
