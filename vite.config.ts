832730aa4c06392586b10c525b60e2685ebc98bf
Yam Borodetsky <hi@yam.codes>
2023-11-25 13:33:02 +0200

    feat: UMD support

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,35 @@ import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
 import dts from 'vite-plugin-dts';
-import packageJson from "./package.json";
+import packageJson from './package.json';
 
 const { name } = packageJson;
-const entryRoot = resolve("src", "components");
-const entry = resolve(entryRoot, "index.ts");
+const entryRoot = resolve('src', 'components');
+const entry = resolve(entryRoot, 'index.ts');
 
 export default defineConfig({
-  plugins: [react(), svgr(), dts({
-    entryRoot,
-    rollupTypes: true
-  })],
+  plugins: [
+    react(),
+    svgr(),
+    dts({
+      entryRoot,
+      rollupTypes: true,
+    }),
+  ],
   // Vite Library Mode
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry,
-      formats: ["es"],
       name,
-      fileName: "index"
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react", "react-dom"],
+      // dependencies that shouldn't be bundled into the library
+      external: ['react', 'react-dom'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        // Global variables to use in the UMD build for externalized dependencies
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
